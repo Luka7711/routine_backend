a3e0ca05cb5f621a819e5d6ab003a337976289cc
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-09-02 11:36:49 -0500

    requiring npm packages in server file

---
+++ server.js
@@ -0,0 +1,8 @@
+const express 			= require('express');
+const app		 		= express();
+const bodyParser 		= require('body-parser');
+const methodOverride 	= require('method-override');
+const cors 				= require('cors');
+const session 			= require('require-session');
+
+
