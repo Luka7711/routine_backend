1e1416b0f4ef6b31852df304744899521ff3a01c
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-09-02 11:41:45 -0500

    middlewares are set up

--- server.js
+++ server.js
@@ -5,4 +5,17 @@ const methodOverride 	= require('method-override');
 const cors 				= require('cors');
 const session 			= require('require-session');
 
+require('dotenv').config();
+require('./db/db');
+
+const PORT = process.env.PORT;
+
+app.use(session({
+	secret:process.env.SESSION_SECRET,
+	resave:false,
+	saveUninitilized:false
+}));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
