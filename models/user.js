a055c70e6f06848fba3739b96fda90fa26f32d70
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-09-02 14:14:12 -0500

    User model added

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Diary = require('./diary');
+
+const userSchema = new mongoose.Schema({
+	username: {type: String, require:true},
+	password: {type: String, require:true},
+	zodiac: {type:String, require:false},
+	
+	movieList:[{
+		type:mongoose.Schema.Types.ObjectId,
+		ref: 'Diary'
+	}]
+})
+
+const User = new mongoose.model('User', userSchema);
+
+module.exports = User;
