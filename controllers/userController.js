0b9330831fab4d2fdf7ffcfb83edbf3287a580a4
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-09-03 10:38:06 -0500

    post route to login user

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,33 +4,41 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 router.post('/register', async(req, res, next) => {
-	
-	const password = req.body.password;
-	const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-	const userDbEntry = {};
-	
-	userDbEntry.username = req.body.username;
-	userDbEntry.password = passwordHash;
 
-	try{
-		const newUser = await User.create(userDbEntry);
-		req.session.logged = true;
-		req.session.userDbId = newUser._id;
-
-		res.json({
-			status:200,
-			data: newUser,
-			message:`Welcome to Daily Routine ${newUser.username}`
-		})
-
-		console.log(`${newUser} added to database`)
-
-	}catch(err){
-		next(err)
+	if(User.findOne({'username':req.body.username}) === true){
 		res.json({
 			status:404,
-			message:'Something went wrong!'
+			message:'Username has been taken, try another'
 		})
+
+	}else{
+		const password = req.body.password;
+		const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+		const userDbEntry = {};
+
+		userDbEntry.username = req.body.username;
+		userDbEntry.password = passwordHash;
+		userDbEntry.zodiac = req.body.zodiac
+
+		try{
+			const newUser = await User.create(userDbEntry);
+			req.session.logged = true;
+			req.session.userDbId = newUser._id;
+
+			res.json({
+				status:200,
+				data: newUser,
+				message:`Welcome to Daily Routine ${newUser.username}`
+			})
+
+		}catch(err){
+			next(err)
+			res.json({
+				status:404,
+				message:'Something went wrong!'
+			})
+		}
+
 	}
 })
 
@@ -52,25 +60,36 @@ router.get('/logout', (req, res) => {
 
 router.post('/login', async(req, res, next) => {
 	try{
-		const user = await User.findOne({'username':req.body.username});
-
-		if(user){
-			if(bcrypt.compareSync(req.body.password, user.password) === true){
+		const foundUser = await User.findOne({'username':req.body.username});
+		console.log('current user')
+		console.log(req.body.password + " ", + " " + foundUser.password);
+		if(foundUser){
+			if(bcrypt.compareSync(req.body.password, foundUser.password) === true){
 				req.session.logged === true;
-				req.session.userDbId === user._id
-
+				req.session.userDbId === foundUser._id
 				res.json({
 					status:200,
-					data: user,
-					message: `Welcome to Routine ${user.username}`
+					data: foundUser,
+					message: `Welcome to Routine ${foundUser.username}`
 				})
 			}else{
 				res.json({
 					status:404,
-					message:'username or password is incorrect, try again'
+					message:'Password is incorrect, try again'
 				})
 			}
+		}else{
+			res.json({
+				status:404,
+				message:'Username or password is incorrect'
+			})
 		}
+	}catch(err){
+		next(err);
+		res.json({
+			status:404,
+			message:'Not able to sign in, something went wrong'
+		})
 	}
 })
 
