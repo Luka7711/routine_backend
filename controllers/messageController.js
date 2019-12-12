9add8c37847deb9d350a1cdb161a862c848d6524
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2019-12-12 13:10:54 -0600

    creating conversation, adding participants

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const Conversation = require('../models/conversation');
 
 router.post('/:author/:receiver', async(req, res, next)=>{
-	console.log('successful chat')
+	//1. create conversation
+	const shipper = await User.findOne({username:req.params.author});
+	const receiver = await User.findOne({username:req.params.receiver});
+	const convers = await Conversation.create(req.body);
+	convers.participants.push(shipper, receiver);
+	convers.save();
+	shipper.conversation.push(convers);
+	shipper.save();
+	receiver.conversation.push(convers);
+	receiver.save();
+	//2. add convoID to user model
+	//3. create message
 })
 
 
