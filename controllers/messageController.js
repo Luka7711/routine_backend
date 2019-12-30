47acacb56a367cb0008b03615492fcad3724c52f
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2019-12-30 13:53:02 -0600

    images

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -16,43 +16,49 @@ router.post('/texting/:user/:convoid', async(req, res, next) => {
 		const message = await Message.create(messageData);
 		message.save();
 
+		res.json({
+			status:200,
+			message: message
+		})
 	}catch(err){
 		console.log(err)
 	}
 		
 })
 
+			//zack <=> dina
 router.post('/:author/:receiver', async(req, res, next)=>{
 	// when contact is chosen, and ready to start to chat
-	// find conversation where author and receiver 
+	// find conversation where participants 1 and 2 has conversation 
 	try{
 		const shipper = await User.findOne({username:req.params.author});
 		const receiver = await User.findOne({username:req.params.receiver});
-		const foundConversation = await Conversation.find({"participants": [shipper.id, receiver.id]})
-		
-		if(foundConversation.length === 0){
-			const newConvers = await Conversation.create(req.body);
+		await Conversation.find({"participants": {"$all" : [shipper.id, receiver.id]} }, async(err, conversation) => {
+			console.log(conversation)
+			if(conversation.length == 0){
+				const newConvers = await Conversation.create(req.body);
 			
-			newConvers.participants.push(shipper, receiver);
-			newConvers.save();
+				newConvers.participants.push(shipper, receiver);
+				newConvers.save();
 			
-			shipper.conversation.push(newConvers);
-			shipper.save();
-			
-			receiver.conversation.push(newConvers);
-			receiver.save();
-	
-			res.json({
+				shipper.conversation.push(newConvers);
+				shipper.save();
+				
+				receiver.conversation.push(newConvers);
+				receiver.save();
+
+				res.json({
+					status:200,
+					conversationData: newConvers.id
+				})
+			}else{
+				res.json({
 				status:200,
-				conversationData: newConvers.id
-			})
-		
-		}else{
-			res.json({
-				status:200,
-				conversationData: foundConversation[0].id
-			})
-		}
+				conversationData: conversation[0]._id,
+				mess: "exist chat"
+				})
+			}
+		})
 	}catch(err){
 		console.log(err)
 	}
@@ -60,9 +66,22 @@ router.post('/:author/:receiver', async(req, res, next)=>{
 });
 
 
-router.get('/my-messages/:convoid', async(req, res, next) => {
+router.get('/my-conversation/:convoid', async(req, res, next) => {
 	try{
-
+		await Message.find({"conversationId" : req.params.convoid}, (err, message) =>{
+			if(err){
+				res.json({
+					status:404,
+					sms: "Something went wrong"
+				})
+			}else{
+				res.json({
+					status:200,
+					messages: message,
+					sms: "Success"
+				})
+			}
+		})
 	}catch(err){
 		next(err)
 	}
